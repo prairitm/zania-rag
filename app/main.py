4bc09568652fb737f42dbc66401ad5ab0c122535
Prairit Mathur <prairit.m@gmail.com>
2024-07-03 17:04:05 +0530

    app title update

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from app.routers import docs
 
 # Initialize the FastAPI application
-app = FastAPI()
+app = FastAPI(title='Zania-RAG-Langchain-OpenAI')
 
 # Include the router for document-related endpoints
 app.include_router(docs.router)
